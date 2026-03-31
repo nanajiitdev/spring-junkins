0830945e4abfb00e99cc8e0ca732e9cdafab52bc
Nanaji Ruttala <nanaji.itdev@gmail.com>
2026-03-31 21:20:49 +0530

    first commit

---
+++ src/test/java/com/mugdhasoft/junkins/api/SpringJunkinsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mugdhasoft.junkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest
+class SpringJunkinsApplicationTests {
+public static Logger logger=LoggerFactory.getLogger(SpringJunkinsApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing....");
+		 assertEquals(true, true);
+	}
+
+}
